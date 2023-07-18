c65d3192124b6cd78a791728c54fc7d6de61ec06
karthikreddy552 <kmadavanaru@gmail.com>
2023-07-18 19:47:31 +0530

    changes done

--- server1/addproduct.js
+++ server1/addproduct.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const connectDB = require("./config/db.config");
-// const MockData1 = require("./models/data1.model");
-const MockData2 = require("./models/data2.model");
+const MockData1 = require("./models/data1.model");
+// const MockData2 = require("./models/data2.model");
 
 
-// const ProductJson = require("./mock_data_1.json");
-const ProductJson = require("./mock_data_2.json");
+const ProductJson = require("./mock_data_1.json");
+// const ProductJson = require("./mock_data_2.json");
 
 
 // to write a bulk data
@@ -13,8 +13,8 @@ const ProductJson = require("./mock_data_2.json");
 const start = async () => {
     try {
         await connectDB(process.env.MONGODB_URL)
-        await MockData2.deleteMany();
-        await MockData2.create(ProductJson);
+        await MockData1.deleteMany();
+        await MockData1.create(ProductJson);
         console.log("success");
     }
     catch (err) {
